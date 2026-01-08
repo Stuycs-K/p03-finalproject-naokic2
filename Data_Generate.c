bfeca81e17ca02b39bf650b96b35ee25bcfd04a3
naokic2 <naokic2@nycstudents.net>
2026-01-07 19:06:27 -0800

    started data generation process

--- Data_Generate.c
+++ Data_Generate.c
@@ -1,1 +1,20 @@
 // generates the random sample of data with a specified y = mx_1 + mx_2 ...mx_n + b equation, then writes it into a file.
+#include <fcntl.h>
+#include <unistd.h>
+double randomDouble(double min, double max){
+    srand(time(NULL));
+    return (double) rand() * (min - max) + min;
+}
+
+int main(argc,char *argv[]){
+    if(argc == 3){
+        points = [1];
+        dims = argv[2];
+    }else{
+        printf("wrong input! Input filename, # data points, dimensions");
+    }
+    double* X;
+    double* y;
+    int rows = load_csv(filename, dims, &X, &y);
+
+}
