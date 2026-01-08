39214d9c6453d0ddb02d0898a23f3af56aaec540
naokic2 <naokic2@nycstudents.net>
2026-01-07 20:17:28 -0800

    finished data generation in array, now writing to file

--- Data_Generate.c
+++ Data_Generate.c
@@ -5,19 +5,25 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include "Data_Generate.h"
+#include <math.h>
 double randomDouble(double min, double max){
     return (double) rand() * (min - max) + min;
 }
 
 double function(double* Weights, double* Bias,int dims){
-    for(int i=0;i<dims;i++){
-        
-    }
+
 }
 
 double NormalDSampler(int std, int mean){ //generates a random number based on a mean and std from the normal distribution
 
-    double
+        double u = (double)rand() / RAND_MAX;
+        double v = (double)rand() / RAND_MAX;
+        while(u ==0.0){ // no errs
+            u = double rand() / RAND_MAX;
+        }
+        double num = sqrt(-2.0 * log(u)) * cos(2.0 * pi * v);
+        return mean + (z * std);
+
 }
 
 
@@ -33,9 +39,10 @@ int main(intargc, char *argv[]){
     srand(time(NULL));
     
 
+    /////////////// DATA GENERATION //////////////////////
     //randomly get true weights and bias
     double* trueWeights = calloc(dims, sizeof(double));
-    double* trueBias = randomDouble(-10,10);
+    double* trueBias = randomDouble(-100,100);
     for(int i=0,i<dims;i++){
         trueWeights[i] = randomDouble(-10,10);
     }
@@ -46,21 +53,28 @@ int main(intargc, char *argv[]){
     printf(" %f",trueBias);
 
     
-    double** dataPoints = calloc(points * sizeof(double*)); //initialize memory for all data
+    double** dataPoints = calloc(points, sizeof(double*)); //initialize memory for all data
     for(int i=0;i<dims;i++){ //initialize memory for each datapoint n dimensins
-        dataPoints[i] = calloc(dims * sizeof(double));
+        dataPoints[i] = calloc(dims, sizeof(double));
     }
-    double* Y = calloc(points * sizeof(double));
+    double* Y = calloc(points, sizeof(double));
+
 
     for(int i=0;i<points;i++){
-
         for(int x=0;x<dims;x++){
-            dataPoints[i][x] = randomDouble(-100,100);
-            Y += dataPoints[i][x] * (trueWeights[x] );
+            dataPoints[i][x] = randomDouble(-10,10);   // random init dataPoints
+            Y += dataPoints[i][x] * (trueWeights[x] + NormalDSampler(0.1,0)); // solve for y while adding random noise to weights
         }
-        Y += trueBias
-
+        Y += trueBias + NormalDSampler(1,0)
     }
+
+
+
+
+
+
+
+
 
 
     //write data into a file!
