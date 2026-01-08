ba96ed296ce5dd6a7f9897396648bdc2a6666976
naokic2 <naokic2@nycstudents.net>
2026-01-07 20:31:22 -0800

    saving to file function finished

--- Data_Generate.c
+++ Data_Generate.c
@@ -33,7 +33,7 @@ int main(intargc, char *argv[]){
         double dims = argv[2];
         char* filename = argv[0]
     }else{
-        printf("wrong input! Input filename, # data points, dimensions");
+        printf("wrong input! Input filename, # data points, dimensions\n");
     }
 
     srand(time(NULL));
@@ -50,37 +50,51 @@ int main(intargc, char *argv[]){
     for(int i=0;i<dims;i++){
         printf("%f * x(%d) + ",trueWeights[i],i+1);
     }
-    printf(" %f",trueBias);
+    printf(" %f\n\n",trueBias);
 
     
     double** dataPoints = calloc(points, sizeof(double*)); //initialize memory for all data
     for(int i=0;i<dims;i++){ //initialize memory for each datapoint n dimensins
         dataPoints[i] = calloc(dims, sizeof(double));
     }
-    double* Y = calloc(points, sizeof(double));
+    
 
 
     for(int i=0;i<points;i++){
-        for(int x=0;x<dims;x++){
+        int Y = 0;
+        for(int x=1;x<=dims;x++){  /// FORMAT IS y =mx +b-----  data[i][0] = y, data[i][1] = x1 ... data[i][dims + 1] = Bias
             dataPoints[i][x] = randomDouble(-10,10);   // random init dataPoints
             Y += dataPoints[i][x] * (trueWeights[x] + NormalDSampler(0.1,0)); // solve for y while adding random noise to weights
         }
-        Y += trueBias + NormalDSampler(1,0)
+        dataPoints[i][0] = Y;
+        dataPoints[i][dims + 1] = trueBias + NormalDSampler(1,0);
+
+
     }
 
+    printf("Data Generation has completed. Data is approx %lf KiloBytes large!\n", (long)points * sizeof(double) / 1000)
+    printf("Now saving data to file!\n")
+    saveData(filename, points,dims,dataPoints);
+    printf("data has been saved to %s",filename);
+
+}
 
 
-
-
-
-
-
-
-
-    //write data into a file!
+void saveData(char* filename, int points, int dims, double** data){//write data into a file!
     int fd = open(filename,O_WRONLY|O_CREAT|O_TRUNC,0644);
     if(fd < 0){
         err();
     }
-
+    char buffer[50000];
+    
+    for(int i=0;i<rows;i++){
+        int count =0;
+        count += sprintf(buffer + offset,"%f",data[i][0]);
+        for(int x=1;x<=dims+1;x++){
+            count += sprintf(buffer+count,"%f",data[i][x]);
+        }
+        count += sprintf(buffer +count,"\n");
+        write(fd, buffer, offset);
+    }
+    close(fd);
 }
