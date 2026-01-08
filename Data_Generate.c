34ae9ff9a93609674b69dcbb28ba235ea75e0f1e
naokic2 <naokic2@nycstudents.net>
2026-01-07 20:43:58 -0800

    started debugging, requires more

--- Data_Generate.c
+++ Data_Generate.c
@@ -4,34 +4,45 @@
 //WRITE ERROR FUNCTIOn---
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+
 #include "Data_Generate.h"
 #include <math.h>
+
+void err(){
+    exit(0);
+}
+
+
 double randomDouble(double min, double max){
-    return (double) rand() * (min - max) + min;
+    return (double) rand() / RAND_MAX * (min - max) + min;
 }
 
-double function(double* Weights, double* Bias,int dims){
 
-}
 
 double NormalDSampler(int std, int mean){ //generates a random number based on a mean and std from the normal distribution
 
         double u = (double)rand() / RAND_MAX;
         double v = (double)rand() / RAND_MAX;
         while(u ==0.0){ // no errs
-            u = double rand() / RAND_MAX;
+            u = (double) rand() / RAND_MAX;
         }
         double num = sqrt(-2.0 * log(u)) * cos(2.0 * pi * v);
-        return mean + (z * std);
+        return mean + (num * std);
 
 }
 
 
-int main(intargc, char *argv[]){
+int main(int argc, char *argv[]){
+    char* filename;
+    double points,dims;
     if(argc == 3){
-        double points = argv[1];
-        double dims = argv[2];
-        char* filename = argv[0]
+        points = argv[1];
+        dims = argv[2];
+        filename = argv[0];
     }else{
         printf("wrong input! Input filename, # data points, dimensions\n");
     }
@@ -41,20 +52,20 @@ int main(intargc, char *argv[]){
 
     /////////////// DATA GENERATION //////////////////////
     //randomly get true weights and bias
-    double* trueWeights = calloc(dims, sizeof(double));
-    double* trueBias = randomDouble(-100,100);
-    for(int i=0,i<dims;i++){
+    double* trueWeights = calloc(dims+1, sizeof(double)); //will have same format as dataPoints array: first entry is empty, weights * dims
+    double trueBias = randomDouble(-100,100);
+    for(int i=1;i<=dims;i++){
         trueWeights[i] = randomDouble(-10,10);
     }
     printf("True equation:\n");
-    for(int i=0;i<dims;i++){
+    for(int i=1;i<dims;i++){
         printf("%f * x(%d) + ",trueWeights[i],i+1);
     }
     printf(" %f\n\n",trueBias);
 
     
     double** dataPoints = calloc(points, sizeof(double*)); //initialize memory for all data
-    for(int i=0;i<dims;i++){ //initialize memory for each datapoint n dimensins
+    for(int i=0;i<points;i++){ //initialize memory for each datapoint n dimensins
         dataPoints[i] = calloc(dims, sizeof(double));
     }
     
@@ -87,14 +98,14 @@ void saveData(char* filename, int points, int dims, double** data){//write data 
     }
     char buffer[50000];
     
-    for(int i=0;i<rows;i++){
+    for(int i=0;i<points;i++){
         int count =0;
         count += sprintf(buffer + offset,"%f",data[i][0]);
         for(int x=1;x<=dims+1;x++){
             count += sprintf(buffer+count,"%f",data[i][x]);
         }
         count += sprintf(buffer +count,"\n");
-        write(fd, buffer, offset);
+        write(fd, buffer, count);
     }
     close(fd);
 }
