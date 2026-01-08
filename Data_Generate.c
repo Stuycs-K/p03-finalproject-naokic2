08a3556bd59ba577aaae5fdd4ec7df6a7d2184a7
naokic2 <naokic2@nycstudents.net>
2026-01-07 19:59:26 -0800

    working on memory management for random data generation:

--- Data_Generate.c
+++ Data_Generate.c
@@ -1,20 +1,59 @@
 // generates the random sample of data with a specified y = mx_1 + mx_2 ...mx_n + b equation, then writes it into a file.
+
+
+//WRITE ERROR FUNCTIOn---
 #include <fcntl.h>
 #include <unistd.h>
 double randomDouble(double min, double max){
-    srand(time(NULL));
     return (double) rand() * (min - max) + min;
 }
 
-int main(argc,char *argv[]){
+double function(double* Weights, double* Bias,int dims){
+    for(int i=0;i<dims;i++){
+        
+    }
+}
+
+int main(intargc, char *argv[]){
     if(argc == 3){
-        points = [1];
-        dims = argv[2];
+        double points = argv[1];
+        double dims = argv[2];
+        char* filename = argv[0]
     }else{
         printf("wrong input! Input filename, # data points, dimensions");
     }
-    double* X;
-    double* y;
-    int rows = load_csv(filename, dims, &X, &y);
+
+    srand(time(NULL));
+    
+
+    //randomly get true weights and bias
+    double* trueWeights = calloc(dims, sizeof(double));
+    double* trueBias = randomDouble(-10,10);
+    for(int i=0,i<dims;i++){
+        trueWeights[i] = randomDouble(-10,10);
+    }
+    printf("True equation:\n");
+    for(int i=0;i<dims;i++){
+        printf("%f * x(%d) + ",trueWeights[i],i+1);
+    }
+    printf(" %f",trueBias);
+
+    
+    double** dataPoints = calloc(points * sizeof(double*)); //initialize memory for all data
+    for(int i=0;i<dims;i++){ //initialize memory for each datapoint n dimensins
+        dataPoints[i] = calloc(dims * sizeof(double));
+    }
+
+
+    for(int i=0;i<points;i++){
+
+    }
+
+
+    //write data into a file!
+    int fd = open(filename,O_WRONLY|O_CREAT|O_TRUNC,0644);
+    if(fd < 0){
+        err();
+    }
 
 }
